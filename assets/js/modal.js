798916c61266eb6aaa4ecf095a0a19fdf0e49365
stevenrobday <stevenrobday@gmail.com>
2019-01-02 16:37:00 -0700

    fixed modal position and background scroll when scrollTop is greater than 0

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,3 +1,4 @@
+var isModal = false;
 var isSuper = false;
 
 var $modalShade = $("#modalShade");
@@ -8,45 +9,36 @@ var $superModal = $("#superModal");
 var $superClose = $("#superClose");
 var $superCloseWrap = $("#superCloseWrap");
 var $dataId;
+var $imgTag;
+var imgRatio;
 var $close = $(".close");
 var $thumbnail = $(".thumbnail");
+var $body = $("body");
 
-$exampleThumb.on("click", function () {
-  $modalShade.fadeIn("fast");
-  $dataId = $("#" + $(this).attr("data-id"));
+function showModal() {
+  $dataId.css('margin-top', 'calc(' + $(document).scrollTop() + 'px + var(--font-size-l) * 2)');
   $dataId.show("fast");
-});
+  $body.css('overflow', 'hidden');
+}
 
-$(".close, #modalShade").on("click", function () {
-  $modalShade.fadeOut("fast");
-  $dataId.hide("fast");
-});
-
-$thumbnail.on("click", function () {
-  var src = $(this).attr("src");
-  var $imgTag = $('<img>');
-  $imgTag.attr("src", src);
-
-  var imgWidth = $(this).width();
-  var imgHeight = $(this).height();
-  var imgRatio = imgWidth / imgHeight;
-  
-  var docWdth = $(document).width();
+function showSuper() {
+  var docWidth = $(document).width();
   var docHeight = $(document).height();
+  var docRatio = docWidth / docHeight;
   
-  if(docHeight >= docWdth){
-    if(docWdth < 400 || imgRatio < 1) {
+  if(docRatio <= 1){
+    if(docWidth < 400 || imgRatio < 1) {
       $imgTag.height("100%");
-      $imgTag.width("65vw");
+      $imgTag.width("75vw");
     }
     else {
       $imgTag.height("100%");
       $imgTag.width("80vw");
     }
   }
-  else if(imgRatio <= 1){
+  else if(imgRatio <= 1 || docRatio >= 2){
     $imgTag.height("calc(100vh - var(--font-size-l) * 4)");
-    $imgTag.width("100%");
+    $imgTag.width("auto");
   }
   else if(imgRatio > 1.5){
     $imgTag.height("100%");
@@ -57,11 +49,41 @@ $thumbnail.on("click", function () {
     $imgTag.width("50vw");
   }
 
+  console.log(docRatio);
+
+  $superModal.css('margin-top', $(document).scrollTop());
+
   isSuper = true;
 
   $superModalImg.html($imgTag);
   $superShade.fadeIn("fast");
   $superModal.show("fast");
+}
+
+$exampleThumb.on("click", function () {
+  isModal = true;
+  $modalShade.fadeIn("fast");
+  $dataId = $("#" + $(this).attr("data-id"));
+  showModal();
+});
+
+$(".close, #modalShade").on("click", function () {
+  isModal = false;
+  $modalShade.fadeOut("fast");
+  $dataId.hide("fast");
+  $body.css('overflow', 'auto');
+});
+
+$thumbnail.on("click", function () {
+  var src = $(this).attr("src");
+  $imgTag = $('<img>');
+  $imgTag.attr("src", src);
+
+  var imgWidth = $(this).width();
+  var imgHeight = $(this).height();
+  imgRatio = imgWidth / imgHeight;
+  
+  showSuper();
 });
 
 $("#superCloseWrap, #superClose, #superShade").on("click", function () {
@@ -72,11 +94,13 @@ $("#superCloseWrap, #superClose, #superShade").on("click", function () {
 
 window.addEventListener('keyup', (e) => {
   if (e.key === "Escape") {
-    if (!isSuper) {
+    if (!isSuper && isModal) {
+      isModal = false;
       $modalShade.fadeOut("fast");
       $dataId.hide("fast");
+      $body.css('overflow', 'auto');
     }
-    else {
+    else if(isSuper){
       isSuper = false;
       $superShade.fadeOut("fast");
       $superModal.hide("fast");
@@ -85,9 +109,10 @@ window.addEventListener('keyup', (e) => {
 });
 
 $(window).resize(function() {
-  if (isSuper) {
-    isSuper = false;
-      $superShade.fadeOut("fast");
-      $superModal.hide("fast");
+  if (isModal) {
+    showModal();
+  }
+  if(isSuper){
+    showSuper();
   }
 });
